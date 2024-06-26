95200cdc3986f51a26922aa78cbfdce51464792f
= <barkataliabro0786@gmail.com>
2024-06-26 00:50:17 -0700

    allow cors

--- index.js
+++ index.js
@@ -14,17 +14,10 @@ import getconversation from "./helper/getconversation.js"
 
 const app = express();
 
-const corsOptions = {
-    origin: 'http://localhost:5173',
-    credentials: true,
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
-  };
-  
-  app.use(cors(corsOptions));
-  
-  app.options('*', cors(corsOptions)); // Enable pre-flight
-  
+app.use(cors({
+    origin:"*",
+    credentials:true,
+}))  
   
   
 dotenv.config();
