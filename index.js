3f516e15fd31896a4583a4379cec5467a36d20f4
= <barkataliabro0786@gmail.com>
2024-06-26 00:53:38 -0700

    cors uni

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from "express"
-import cors from "cors"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 import userrouter from "./Router/index.js"
@@ -22,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api", userrouter)
 app.get("/",(req,res)=>{
-    res.send("hello api is working cors changing options")
+    res.send("hello api is workingcor uni")
 })
 
 const connect = () => {
